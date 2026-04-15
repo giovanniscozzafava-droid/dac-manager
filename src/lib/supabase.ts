6a0ca264fce2efe4bfe4f4dc95270777ab2ce9de
Giovanni Scozzafava <giovanniscozzafava@MacBookPro.homenet.telecomitalia.it>
2026-04-15 12:04:48 +0200

    Fix: disable Supabase auth lock

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,15 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://yyjhuvftcwvnxlskvjne.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5amh1dmZ0Y3d2bnhsc2t2am5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNDg1MDUsImV4cCI6MjA5MTcyNDUwNX0.MEohst7ka_cg_XtwLIbCRbxphxQghqYdFBDSkWMftas'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5amh1dmZ0Y3d2bnhsc2t2am5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNDg1MDUsImV4cCI6MjA5MTcyNDUwNX0.MEohst7ka_cg_XtwLIbCRbxphxQghqYdFBDSkWMftas',
+  {
+    auth: {
+      storageKey: 'dac-auth',
+      flowType: 'implicit',
+      detectSessionInUrl: true,
+      persistSession: true,
+      autoRefreshToken: true,
+      lock: { enabled: false }
+    }
+  }
 )
